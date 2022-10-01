a7e8b8169871f60822541846d3cbc3b6912d8951
Your Name <you@example.com>
2022-10-02 01:09:36 +0200

    dev-02102022 Met en place la config prettier eteslint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, //pathMatch
+    { path: 'home', component: HomeComponent },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
